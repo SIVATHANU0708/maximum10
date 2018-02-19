c00d0d567498432fa964bab3a2460da199a15fd4
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-19 15:48:20 +0530

    maximum10.C
    maximum element among ten number

--- maximum10.C
+++ maximum10.C
@@ -1,5 +1,22 @@
+
+  
 #include<stdio.h>
 int main()
 {
-int n,i,a[100],j;
-printf("enter the no of element:");
+int n=10,i,a[10],maximum;
+printf("enter the element:");
+  for(i=0;i<=10;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  maximum=a[0];
+  for(i=0;i<=10;i++)
+  {
+    if(a[i]<maximum)
+    {
+      maximum=a[i];
+    }
+  }
+  printf("the maximum element among ten number is %d",maximum);
+  retutn 0;
+}
